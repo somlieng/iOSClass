5a8471056bc89332295b7099a93acaa26989a4db
Som <Som@soms-macbook-air.local>
2016-09-08 18:41:42 -0400

    Initial Commit

---
+++ Hello I'm SomTests/Hello_I_m_SomTests.swift
@@ -0,0 +1,36 @@
+//
+//  Hello_I_m_SomTests.swift
+//  Hello I'm SomTests
+//
+//  Created by Som on 9/8/16.
+//  Copyright © 2016 Som. All rights reserved.
+//
+
+import XCTest
+@testable import Hello_I_m_Som
+
+class Hello_I_m_SomTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
